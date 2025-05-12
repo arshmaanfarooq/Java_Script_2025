d0587ef24adf9ebb7d2a32c545269bdb1f6e73a4
arshmaanfarooq <arshmaanmemon77@gmail.com>
2025-05-12 19:26:32 +0500

    2 folder

---
+++ assigment 1/index.js
@@ -0,0 +1,4 @@
+alert("ERROR ! Please enter a valid password");
+var arshmaan = "Welcome to JS Land. \nHappy Coding"
+alert(arshmaan)
+
---
+++ assigment 2/example/app.js
@@ -0,0 +1,13 @@
+// Assigment
+var height = Number(prompt ("what is the height") );
+var widht = Number(prompt ("what is the widht"));
+alert ( height * widht )
+var perimeter = ( 2 * height + widht);
+alert ( "the perameter is " + perimeter);
+
+// Calculate the volume and surface Area of a box using javascript
+var height = Number(prompt("What is the Height of cuboid"))
+var widht = Number(prompt("What is the width of cuboid"))
+var length = Number (prompt("What is the Length of cuboid"))
+alert ( "Volume of Cuboid is " + height * widht * length )
+alert ("surface area of Cuboid is " + length * widht + widht * height + height * length)
